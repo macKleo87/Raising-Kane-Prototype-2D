2f91677cb8502c1c78d07ed10078935327e47fc6
Rachel <tailsnick@yahoo.com>
2019-09-22 15:57:15 -0600

    New Hallway Scene with New Movement Controls
    To move alternate from right to left to move forward. (Used for the boss chase scene)

---
+++ CapstonePrototype/Assets/Scripts/HallwayMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed = 5.5f;
+    public float delay = .005f;
+
+    private bool leftPress = false;
+    private bool rightPress = false;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2();
+
+        //Allow a bit of distance on "D" key press before disabling it and renabling "A" key
+        if (Input.GetAxis("Horizontal") > 0 && rightPress == false) //&& checkEdge2(new Vector2(7.5f, -3.5f)))
+        {
+            movement.x = speed;
+            StartCoroutine(KeyPressDelay());
+            rightPress = true;
+            leftPress = false;
+        }
+        //Allow a bit of distance on "A" key press before disabling it and renabling "D" key
+        if (Input.GetAxis("Horizontal") < 0 && leftPress == false)// && checkEdge(new Vector2(-7.5f, -1.5f)))
+        {
+            movement.x = speed;
+            StartCoroutine(KeyPressDelay());
+            rightPress = false;
+            leftPress = true;
+        }
+        
+    }
+
+    IEnumerator KeyPressDelay()
+    {
+        rb2d.velocity = movement;
+        yield return new WaitForSecondsRealtime(delay);
+        
+    }
+}
